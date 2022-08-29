fab147ce310982ac189b009200626f9a8fabd2a6
Gabriel Riquelme <griquelme.chm@gmail.com>
2022-08-29 14:11:08 -0300

    Working version of the multiplet annotation algorithm.

--- src/nmrtools/multiplet.py
+++ src/nmrtools/multiplet.py
@@ -1,17 +1,64 @@
 import numpy as np
-from itertools import product
-from math import log2, log10
-from .utils import lorentz
+from itertools import permutations
+from math import log2
+from .utils import fid, find_closest, lorentz
 from sklearn.utils.extmath import cartesian
 from typing import List, Optional, Tuple
 
 
-def annotate_multiplet(f: np.ndarray, height: np.ndarray) -> List[List[float]]:
-    max_delta = 4
-    candidates = _find_candidate_heights(height, max_delta)
+def simulate_frequency(
+    f: np.ndarray, j_list: List[float], t2: float, f0: float = 0.0
+) -> np.ndarray:
+    result = np.zeros_like(f)
+    j_tol = f[1] - f[0]
+    f_peaks, h_peaks = _j_list_to_height(j_list, j_tol)
+    for fp, hp in zip(f_peaks, h_peaks):
+        result += hp * lorentz(f, fp + f0, t2)
+    return result
+
+
+def simulate_time(
+    t: np.ndarray, j_list: List[float], t2: float, f0: float = 0, phase: float = 0.0
+) -> np.ndarray:
+    result = np.zeros(t.size, dtype="complex128")
+    j_tol = 1 / t[-1]
+    f_peaks, h_peaks = _j_list_to_height(j_list, j_tol)
+    for fp, hp in zip(f_peaks, h_peaks):
+        result += hp * fid(t, t2, fp + f0, phase)
+    return result
+
+
+def annotate(
+    f: np.ndarray, height: np.ndarray, f_tol: float, max_perturbation: int = 1
+) -> List[List[float]]:
+    """
+    Finds the couplings constants for a multiplet.
+
+
+    Parameters
+    ----------
+    f : np.ndarray
+        sorted frequency of the multiplet peaks.
+    height : np.ndarray
+        Intensity of the peaks.
+    f_tol : float
+        If two peaks in a multiplet are closer than this value, the peaks are
+        merged.
+    max_perturbation : int, default=1
+        Tolerance to add perturbations to the multiplets. See the notes for an
+        explanation of its usage.
+
+    Returns
+    -------
+    annotations : List[List[float]]
+        List of valid annotations. Each element is a list of coupling constants
+        that give place to the multiplet
+
+    """
+    candidates = _find_candidate_heights(height, max_perturbation)
     valid_annotations = list()
     for c_height in candidates:
-        j_list = _multiplet_to_j_list(f, c_height)
+        j_list = _multiplet_to_j_list(f, c_height, f_tol)
         if j_list is not None:
             valid_annotations.append(j_list)
     return valid_annotations
@@ -32,7 +79,7 @@ def _is_symmetric(height: np.ndarray) -> bool:
     """
     q, r = divmod(height.size, 2)
     left = height[:q]
-    right = height[height.size:q - 1 + r:-1]
+    right = height[height.size: q - 1 + r: -1]
     return np.array_equal(left, right)
 
 
@@ -60,35 +107,45 @@ def _find_candidate_heights(height, max_delta: int) -> List[np.ndarray]:
 
     # check if total intensity is a power of 2
     # if not, add perturbations until total int is a power of 2
-    n = height.sum()
-    closest_power_of_2 = 2 ** round(log2(n))
-    delta = n - closest_power_of_2
+    valid_deltas = _get_valid_deltas(height, max_delta)
+    candidates = list()
+    # add perturbations of +/- 1 to height to take into account errors in height
+    for delta in valid_deltas:
+        for n_pos in range(max_delta + 1):
+            for n_neg in range(max_delta + 1 - n_pos):
+                n = n_pos + n_neg
+                if (n_pos - n_neg) == delta:
+                    pert_values = [-1] * n_neg + [1] * n_pos
+                    pert_values = np.array(pert_values, dtype=height.dtype)
+                    for perm_index in permutations(range(height.size), r=n):
+                        perm_index = list(perm_index)
+                        pert = height.copy()
+                        pert[perm_index] += pert_values
 
-    if abs(delta) <= max_delta:
-        perturbations = cartesian([[-1, 0, 1] for _ in range(height.size)])
-        perturbations_sum = perturbations.sum(axis=1)
-        perturbations_abs_sum = np.abs(perturbations).sum(axis=1)
-        valid_mask = (
-            (perturbations_sum == delta) & (perturbations_abs_sum <= max_delta)
-        )
-        perturbations = perturbations[valid_mask, :]
-        candidates = list(perturbations + height)
-    elif n == closest_power_of_2:
-        candidates = [height]
-    else:
-        candidates = list()
+                        # check min values at start/end and multiplet symmetry
+                        check_boundaries = (pert[0] == 1) and (pert[-1] == 1)
+                        all_pos = (pert > 0).all()
 
-    # check symmetric multiplet
-    candidates = [x for x in candidates if _is_symmetric(x)]
-
-    # check minimums at the extremes
-    candidates = [x for x in candidates if ((x[0] == 1) and (x[-1] == 1))]
-
+                        if check_boundaries and _is_symmetric(pert) and all_pos:
+                            candidates.append(pert)
     return candidates
 
 
+def _get_valid_deltas(height: np.ndarray, max_delta: int) -> List[int]:
+    total_height = height.sum()
+    max_exponent = round(log2(total_height)) + 2
+    valid_deltas = list()
+    for k in range(max_exponent):
+        power = 2**k
+        delta = power - total_height
+        if abs(delta) <= max_delta:
+            valid_deltas.append(delta)
+    return valid_deltas
+
+
 def _j_list_to_height(
-        j_list: List[float], j_tol: float) -> Tuple[np.ndarray, np.ndarray]:
+    j_list: List[float], tol: float
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Converts a list of coupling constants into a vector of peak frequencies and
     heights.
@@ -96,7 +153,7 @@ def _j_list_to_height(
     Parameters
     ----------
     j_list : list[float]
-    j_tol : float
+    tol : float
         Tolerance to merge close frequency values.
 
     Returns
@@ -108,43 +165,67 @@ def _j_list_to_height(
     """
 
     # find all frequency values
-    combinations = cartesian([[-0.5, 0.5] for _ in range(len(j_list))])
-    # Rounds to at least the same number of decimals than j_tol
-    n_round = max(1, -round(log10(j_tol)) + 1)
-    peak_combs = (combinations * j_list).sum(axis=1).round(n_round)
-    peak, height = np.unique(peak_combs, return_counts=True)
-
-    # merge close peaks
-    merge_index = np.where(np.diff(peak) < j_tol)[0]
-    while merge_index.size:
-        x0 = peak[merge_index]
-        x1 = peak[merge_index + 1]
-        y0 = height[merge_index]
-        y1 = height[merge_index + 1]
-        y_sum = y0 + y1
-        # merged freq is the weighted average of the merged peaks
-        # merged height is the total height of each peak
-        peak[merge_index + 1] = (x0 * y0 + x1 * y0) / y_sum
-        height[merge_index + 1] = y_sum
-        np.delete(peak, merge_index)
-        np.delete(height, merge_index)
-        merge_index = np.where(np.diff(peak) < j_tol)[0]
-    return peak, height
+    n_j = len(j_list)
+    if n_j:
+        combinations = cartesian([[-0.5, 0.5] for _ in range(n_j)])
+        freq_combinations = np.sort((combinations * j_list).sum(axis=1))
+        freq = list()
+        height = list()
+        f_next = freq_combinations[0]
+        h_next = 0
+        for f in freq_combinations:
+            df = f - f_next
+            # merge close peaks
+            if df < tol:
+                f_next = (f_next * h_next + f) / (h_next + 1)
+                h_next += 1
+            else:
+                freq.append(f_next)
+                height.append(h_next)
+                f_next = f
+                h_next = 1
+        freq.append(f_next)
+        height.append(h_next)
+        freq = np.array(freq)
+        height = np.array(height)
+    else:
+        freq = [0]
+        height = [1]
+    return freq, height
 
 
 def _multiplet_to_j_list(
-    f: np.ndarray, height: np.ndarray) -> Optional[List[float]]:
-    return [1.0]
+    f: np.ndarray, height: np.ndarray, j_tol: float
+) -> Optional[List[float]]:
+    n = f.size
+    f0 = f[0]
+    j_list = list()
+    rem_height = height
+    for k in range(1, n):
+        nj = rem_height[k]
 
+        if nj < 1:
+            continue
 
-def simulate_multiplet_frequency(
-    f: np.ndarray, j: list, delta: float, t2: float, normalize=True
-):
-    result = np.zeros_like(f)
-    j = np.array(j)
-    for p in product([-0.5, 0.5], repeat=j.size):
-        mult_omega = (j * p).sum() + delta
-        result += lorentz(f, mult_omega, t2)
-    if normalize:
-        result /= max(result)
-    return result
+        # add new j values
+        df = f[k] - f0
+        j_list.extend([df] * nj)
+
+        # subtract area from j candidates
+        fq, hq = _j_list_to_height(j_list, j_tol)
+        fq += f0 - fq[0]
+        closest = find_closest(f, fq)
+        if closest.size <= f.size:
+            rem_height = height.copy()
+            rem_height[closest] -= hq
+            if (rem_height == 0).all():
+                # all j found
+                break
+            elif (rem_height < 0).any():
+                # invalid multiplet
+                j_list = None
+                break
+        else:
+            j_list = None
+            break
+    return j_list
--- tests/test_multiplet.py
+++ tests/test_multiplet.py
@@ -73,64 +73,78 @@ def test__find_candidate_heights_symmetric_triplet():
 
 
 def test__find_candidate_heights_asymmetric_quartet():
-    height = np.array([1, 2, 2, 2])
+    height = np.array([1, 3, 3, 2])
     max_delta = 1
     result = m._find_candidate_heights(height, max_delta)
-    expected = np.array([1, 2, 2, 1])
+    expected = np.array([1, 3, 3, 1])
     assert len(result) == 1
     assert np.array_equal(result[0], expected)
 
 
 def test__find_candidate_heights_asymmetric_ddd():
-    height = np.array([1, 1, 1, 1, 2, 1])
+    height = np.array([1, 1, 1, 1, 2, 1, 1, 1])
     max_delta = 1
     result = m._find_candidate_heights(height, max_delta)
-    expected = np.array([1, 1, 1, 1, 1, 1])
+    expected = np.array([1, 1, 1, 1, 1, 1, 1, 1])
     assert len(result) == 1
     assert np.array_equal(result[0], expected)
 
 
 # all tests for _j_list_to_height should return a valid multiplet
-
-def test__j_list_to_height_doublet():
-    j_list = [4.0]
-    j_tol = 0.5
-
-    f, height = m._j_list_to_height(j_list, j_tol)
-    expected_f = np.array([-2.0, 2.0])
-    expected_height = np.array([1, 1])
+@pytest.mark.parametrize(
+    "j_list,tol,expected_f,expected_height",
+    [
+        ([], 0.5, [0], [1]),  # singlet
+        ([4.0], 0.5, [-2.0, 2.0], [1, 1]),      # doublet
+        ([4.0, 4.0], 0.5, [-4.0, 0.0, 4.0], [1, 2, 1]),     # triplet
+        ([4.0, 4.0, 4.0], 0.5, [-6.0, -2.0, 2.0, 6.0], [1, 3, 3, 1]),  # quartet
+        ([4.0, 7.0], 0.5, [-5.5, -1.5, 1.5, 5.5], [1, 1, 1, 1])     # dd
+    ]
+)
+def test__j_list_to_height(j_list, tol, expected_f, expected_height):
+    f, height = m._j_list_to_height(j_list, tol)
+    expected_f = np.array(expected_f)
+    expected_height = np.array(expected_height)
     assert np.array_equal(expected_height, height)
     assert np.allclose(expected_f, f)
 
 
-def test__j_list_to_height_triplet():
-    j_list = [4.0, 4.0]
+def test__multiplet_to_j_list_single_peak():
+    f = np.array([0.0])
+    height = np.array([1])
+    expected = list()
     j_tol = 0.5
-
-    f, height = m._j_list_to_height(j_list, j_tol)
-    expected_f = np.array([-4.0, 0.0, 4.0])
-    expected_height = np.array([1, 2, 1])
-    assert np.array_equal(expected_height, height)
-    assert np.allclose(expected_f, f)
+    result = m._multiplet_to_j_list(f, height, j_tol)
+    assert expected == result
 
 
-def test__j_list_to_height_quartet():
-    j_list = [4.0, 4.0, 4.0]
+@pytest.mark.parametrize(
+    "f,height,j_expected",
+    [
+        ([0.0, 5.0], [1, 1], [5.0]),    # doublet
+        ([0.0, 5.0, 10.0], [1, 2, 1],  [5.0, 5.0]),     # triplet
+        ([0.0, 5.0, 10.0, 15.0], [1, 3, 3, 1], [5.0, 5.0, 5.0]),    # quartet
+        ([0.0, 5.0, 7.0, 12.0], [1, 1, 1, 1], [5.0, 7.0])   # double doublet
+    ]
+)
+def test__multiplet_to_j_list_valid_multiplet(f, height, j_expected):
+    f = np.array(f)
+    height = np.array(height)
     j_tol = 0.5
-
-    f, height = m._j_list_to_height(j_list, j_tol)
-    expected_f = np.array([-6.0, -2.0, 2.0, 6.0])
-    expected_height = np.array([1, 3, 3, 1])
-    assert np.array_equal(expected_height, height)
-    assert np.allclose(expected_f, f)
+    result = m._multiplet_to_j_list(f, height, j_tol)
+    assert np.allclose(j_expected, result)
 
 
-def test__j_list_to_height_double_doublet():
-    j_list = [4.0, 7.0]
+@pytest.mark.parametrize(
+    "f,height",
+    [
+        ([0.0, 5.0, 10.0], [1, 1, 1]),
+        ([0.0, 5.0, 10.0, 15.0], [1, 7, 7, 1])
+    ]
+)
+def test__multiplet_to_j_list_invalid_multiplet(f, height):
+    f = np.array(f)
+    height = np.array(height)
     j_tol = 0.5
-
-    f, height = m._j_list_to_height(j_list, j_tol)
-    expected_f = np.array([-5.5, -1.5, 1.5, 5.5])
-    expected_height = np.array([1, 1, 1, 1])
-    assert np.array_equal(expected_height, height)
-    assert np.allclose(expected_f, f)
+    result = m._multiplet_to_j_list(f, height, j_tol)
+    assert result is None
